3bcbf14e07c3d029ea23bfe7ddfa1f50830be5b7
syama venkatesh <venkatesh@actimize.in>
2024-08-20 14:25:35 +0530

    second changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,7 @@
-set -o 
 
-bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+set -o errexit
+
+bundle install --gemfile="Gemfile-2.6.5"
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
